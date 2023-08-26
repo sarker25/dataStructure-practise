c11c0308f9c089f20ddaf8056e5b32b0ac5e0460
sarker25 <shahadathbinoy25@outlook.com>
2023-08-26 16:18:39 +0600

    feat <linked-list> : implement push method in linked-list

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -1,7 +1,20 @@
+const Node = require("./node");
 class LinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
     this.size = 0;
+  }
+
+  push(val) {
+    const node = new Node(val);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.size++;
   }
 }
