ae2f24e16856107d63d0f764318b2da20e529817
sarker25 <shahadathbinoy25@outlook.com>
2023-08-30 22:11:14 +0600

    refactor <linked list> : link list implemntation has changed

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -6,95 +6,46 @@ class Linkedlist {
     this.tail = null;
     this.size = 0;
   }
-
   push(val) {
-    let newNode = new Node(val);
+    const newNode = new Node(val);
     if (!this.head) {
       this.head = newNode;
-      this.tail = this.head;
+      this.tail = newNode;
     } else {
       this.tail.next = newNode;
       this.tail = newNode;
     }
     this.size++;
   }
-
   pop() {
     if (!this.head) {
-      return "list is empty";
-    }
-    let current = this.head;
-    let newTail = current;
-    while (current.next) {
-      newTail = current;
-      current = current.next;
-    }
-    newTail.next = null;
-    this.tail = newTail;
-    this.size--;
-    if (this.size === 0) {
-      this.head = null;
-      this.tail = null;
-    }
-    return current.val;
-  }
-
-  shift() {
-    if (!this.head) {
-      return undefined;
-    }
-    let current = this.head;
-    this.head = current.next;
-    this.size--;
-    if (this.size === 0) {
-      this.head = null;
-      this.tail = null;
-    }
-  }
-
-  unshift(val) {
-    let newNode = new Node(val);
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
+      console.log("List is empty");
       return;
     }
-    let currentHead = this.head;
-    newNode.next = currentHead;
-    this.head = newNode;
-    this.size++;
-  }
-
-  get(index) {
-    if (index < 0 || index > this.size) {
-      console.log("wrong index!! Please check again");
+    let top = this.head;
+    this.head = this.head.next;
+    top.next = null;
+    this.size--;
+    if (this.size === 0) {
+      this.tail = null;
     }
+    console.log(top.val);
+    return top.val;
   }
 
   print() {
-    let arr = [];
     let current = this.head;
+    let value = [];
     while (current) {
-      arr.push(current.val);
+      value.push(current.val);
       current = current.next;
     }
-    console.log(arr);
+    console.log(value.join(" "));
   }
 }
-let list1 = new Linkedlist();
-list1.push(10);
-list1.push(20);
-list1.push(30);
-list1.push(40);
-list1.print();
-console.log("=================");
-// let pop = list1.pop();
-// console.log(pop);
-list1.shift();
-console.log("=================");
-list1.print();
-console.log("=================");
-list1.unshift(50);
-list1.print();
-list1.get(5);
-list1.get(-2);
+const list = new Linkedlist();
+list.push(10);
+list.push(20);
+list.print();
+list.pop();
+list.print();
