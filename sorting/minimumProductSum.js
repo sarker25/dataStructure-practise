76be9425e397b4d87448c3d22348e8f0f6de5a98
sarker25 <shahadathbinoy25@outlook.com>
2023-09-10 12:56:35 +0600

    refactor : minimum product sum

--- sorting/minimumProductSum.js
+++ sorting/minimumProductSum.js
@@ -1,15 +1,23 @@
+/**
+ * {problem description}
+ * You are given an array of integers of size N.N is even. Initially, your score is 0.You   do the following operation until the array becomes empty
+ *
+ * For Each operation, select two integers from the array, remove them, multiply them, and add them to your score.
+ */
+
 let input = "6\n10 8 14 3 10 2";
 let inputs = input.split("\n");
 let n = parseInt(inputs[0]);
 let arr = inputs[1].split(" ").map((x) => parseInt(x));
-console.log(arr);
 
 function minimumProductSum(arr) {
   arr.sort((a, b) => a - b);
   let sum = 0;
-  for (let i = 0; i < arr.length / 2; i++) {
-    sum += arr[i] * arr[arr.length - i - 1];
+  while (arr.length > 0) {
+    let first = arr.shift();
+    let last = arr.pop();
+    sum += first * last;
   }
   console.log(sum);
 }
-minimumProductSum(arr);
+minimumProductSum([10, 8, 14, 3, 10, 2]);
